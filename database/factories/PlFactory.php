651f59a174f042258f5200e2a83f8839803e6838
saber <saber.tabatabaee@gmail.com>
2021-08-23 17:31:15 +0430

    init

---
+++ database/factories/PlFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Pl;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PlFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Pl::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
