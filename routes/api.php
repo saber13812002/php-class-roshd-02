880724529ea45a0359eb31ac423005f86885b1bd
saber <saber.tabatabaee@gmail.com>
2021-08-25 16:57:14 +0430

    init

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('test', function (Request $request) {
+    return $request;
+});
+
+Route::post('test/post', function (Request $request) {
+    return $request;
+});
+
+Route::get('saber','Controller@method');
